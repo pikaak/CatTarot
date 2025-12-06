c6bbce98ef6a41cd6bbc34e850edcb0e98027033
pikaak <kachu00@gmail.com>
2025-12-06 22:28:42 +0900

    Revert "Update QuestionInput.tsx"
    This reverts commit e2669de9a0b1783d549814e90e52f9c0f702fce7.

--- client/src/components/QuestionInput.tsx
+++ client/src/components/QuestionInput.tsx
@@ -28,13 +28,14 @@ export default function QuestionInput({
 
   return (
     <div className="space-y-4">
-
-      {/* 광고 영역 (높이 강제 X, flex 사용 X) */}
-      <div className="w-full max-w-md mx-auto">
-        <GoogleAdBanner className="min-h-[100px]" />
+      {/* 광고 배너 영역 */}
+      <div className="h-24 border-t flex items-center justify-center">
+        <div className="w-full max-w-md mx-auto">
+          <GoogleAdBanner />
+        </div>
       </div>
 
-      {/* 입력 영역 */}
+      {/* 질문 입력 영역 */}
       <div className="flex gap-2 items-center">
         <Input
           type="text"
